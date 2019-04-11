2ef53669531e4741459f70f22a4d284fd9d7a045
Yudishthir Lokhande <yudishthir11@gmail.com>
2019-04-11 10:45:17 +0530

    Updated script.js

--- Dbsproject/WebContent/script.js
+++ Dbsproject/WebContent/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
   $("#show").click(function(){
     $("#hidden").show();
+    $("#hidden1").hide();
   });
 
    $("#close").click(function(){
     $("#hidden").hide();
   });
+
+   $("#show1").click(function(){
+    $("#hidden1").show();
+    $("#hidden").hide();
+  });
+
+   $("#close1").click(function(){
+    $("#hidden1").hide();
+  });
 });
+
